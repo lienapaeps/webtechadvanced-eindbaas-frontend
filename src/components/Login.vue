57924edf0100b0c5492290fc89b8d15d1369dd59
Rix11-H <60402558+Rix11-H@users.noreply.github.com>
2022-04-28 12:12:08 +0200

    add: deploy basic sass structure

---
+++ src/components/Login.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from 'vue'
+
+</script>
+
+<template>
+  <main>
+    <h1>Login to Jackpot</h1>
+    <div>
+      <label for=""></label>
+      <input type="text" id="username" name="username" placeholder="Username">
+      <input type="password" id="password" name="password" placeholder="Password">
+      <button type="submit">Login</button>
+    </div>
+    <div>
+      <p>
+        Don't have an account yet? | <a href="#">Register</a>
+      </p>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
