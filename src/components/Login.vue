b1db05d5603611240b125478318df283d3debfbe
lienapaeps <paepsliena@gmail.com>
2022-05-23 11:04:14 +0200

    feat: login with token

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,5 +1,49 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
+
+let user = ref({});
+
+function login() {
+  let btnLogin = document.querySelector(".button--login").addEventListener("click", function () {
+    console.log("clicked");
+    let username = document.querySelector("#username").value;
+    let password = document.querySelector("#password").value;
+
+    fetch("http://localhost:3002/api/v1/users/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        username: username,
+        password: password
+      })
+    })
+      .then(response => response.json())
+      .then((json) => {
+        user.value = {
+          username: username,
+          password: password
+        };
+        if (json.status === "Success") {
+          let token = json.data.token;
+          localStorage.setItem("token", token);
+
+          window.location.href = "#/home";
+        } else {
+          let feedback = document.querySelector(".alert");
+          feedback.textContent = "Log in failed";
+          feedback.classList.remove("hidden");
+          feedback.style.backgroundColor = "red";
+        }
+      })
+      .catch(error => console.log(error));
+  });
+}
+
+onMounted(() => {
+  login();
+});
 
 </script>
 
@@ -8,6 +52,9 @@ import { ref } from 'vue'
     <h1>Welcome to Jackpot</h1>
     <div class="form form--login">
       <h2>Login</h2>
+      <div class="alert hidden">
+        Here is some feedback
+      </div>
       <div class="form__row">
         <label for="username">Username</label>
         <input type="text" id="username" name="username" placeholder="Username">
@@ -16,7 +63,7 @@ import { ref } from 'vue'
         <label for="password">Password</label>
         <input type="password" id="password" name="password" placeholder="Password">
       </div>
-      <button type="submit" class="button button--login">Login</button>
+      <button class="button button--login">Log in</button>
       <p>
         Don't have an account yet? | <a href="#/register">Register</a>
       </p>
@@ -27,5 +74,17 @@ import { ref } from 'vue'
 <style scoped>
 a {
   color: #42b983;
+}
+
+.hidden {
+  display: none;
+}
+
+.alert {
+  margin-top: 1em;
+  background-color: #B4C4E7;
+  padding: 0.5em;
+  margin-bottom: .5em;
+  color: #32549d;
 }
 </style>
