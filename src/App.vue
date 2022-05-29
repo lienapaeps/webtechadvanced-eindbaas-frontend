6b28e207f9214945f5d331726f21b1a775fb1e0d
lienapaeps <paepsliena@gmail.com>
2022-05-29 18:20:04 +0200

    feat: add procfile for heroku

--- src/App.vue
+++ src/App.vue
@@ -29,6 +29,15 @@ let currentView = computed(() =>
   routes[currentPath.value.slice(1) || '/'] || Home);
 // console.log(currentView);
 
+//primus live
+// primus = Primus.connect("http://localhost:3000", {
+//   reconnect: {
+//     max: Infinity // Number: The max delay before we try to reconnect.
+//     , min: 500 // Number: The minimum delay before we try reconnect.
+//     , retries: 10 // Number: How many times we should try to reconnect.
+//   }
+// });
+
 onMounted(() => {
   window.addEventListener('hashchange', () => {
     currentPath.value = window.location.hash;
