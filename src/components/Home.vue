f64b224da6e2bbf4d0e67ad540b724b9878324ff
Rix11-H <60402558+Rix11-H@users.noreply.github.com>
2022-05-29 21:48:45 +0200

    fix: try get user balance

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -66,7 +66,7 @@ onMounted(() => {
     <div class="coin">
       <img class="coin__img" src="../assets/Vector.svg" alt="Coin">
     </div>
-    <div class="balance" v-for="user, index in users" v-bind:key="index">
+    <div class="balance">
       <h2 class="balance__text">Current balance</h2>
       <h3 class="balance__number">{{ user.balance }}</h3>
     </div>
